71b3af251f687ee616ddc834e7e2198656b919de
Shruthaja <mail.shruthaja@gmail.com>
2023-06-07 20:16:19 -0500

    added startup.txt

--- app.py
+++ app.py
@@ -110,5 +110,4 @@ def change():
     return render_template("change.html",row=row,row2=row2)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
